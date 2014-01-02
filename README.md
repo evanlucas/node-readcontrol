5c8b7cd083695fac8dfc6cd561313006e2a87032
Evan Lucas <evanlucas@me.com>
2014-01-02 03:44:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# readcontrol
+
+A way to read Debian control files
+
+## Installation
+```sh
+npm install --save readcontrol
+```
+
+## Unit Tests
+
+To run tests:
+
+```bash
+$ npm test
+```
+
+## API
+
+### control.version
+
+  Exposes the module version
+  
+  Version:  `0.0.0`
+
+***
+
+### control.read()
+
+  Parses the file at the given _fp_
+  
+  Example
+  
+```javascript
+ control.read('./control', function(err, out) {
+   if (err) throw err
+   console.log(out)
+   // => {
+   // =>   package: 'com.curapps.test',
+   // =>   name: 'Test',
+   // =>   depends: ['mobilesubstrate', 'preferenceloader'],
+   // =>   version: '2.0.0',
+   // =>   architecture: 'iphoneos-arm',
+   // =>   description: 'This is a test',
+   // =>   maintainer: 'Evan Lucas',
+   // =>   author: 'Evan Lucas',
+   // =>   homepage: 'http://example.com',
+   // =>   section: 'Tweaks'
+   // => }
+ })
+```
+
+##### Params
+| Name | Type(s) | Description |
+| ---- | ------- | ----------- |
+| fp | String | The file path |
+| cb | Function | function(err, obj) |
+
+
+***
+
+### control.readSync()
+
+  Parses the file at the given _fp_
+  
+  Example
+  
+```javascript
+ var out = control.readSync('./control')
+ console.log(out)
+ // => {
+ // =>   package: 'com.curapps.test',
+ // =>   name: 'Test',
+ // =>   depends: ['mobilesubstrate', 'preferenceloader'],
+ // =>   version: '2.0.0',
+ // =>   architecture: 'iphoneos-arm',
+ // =>   description: 'This is a test',
+ // =>   maintainer: 'Evan Lucas',
+ // =>   author: 'Evan Lucas',
+ // =>   homepage: 'http://example.com',
+ // =>   section: 'Tweaks'
+ // => }
+```
+
+##### Params
+| Name | Type(s) | Description |
+| ---- | ------- | ----------- |
+| fp | String | The file path |
+
+
+***
---
+++ test/test.js
@@ -0,0 +1,83 @@
+var crw = require('../lib/index')
+  , path = require('path')
+  , should = require('should')
+
+describe('controlrw', function() {
+  describe('read()', function() {
+    describe('valid control file', function() {
+      it('should return null, object', function(done) {
+        var controlFile = path.join(__dirname, 'control')
+        crw.read(controlFile, function(err, out) {
+          should.ifError(err)
+          out.should.be.type('object')
+          out.should.haveOwnProperty('package')
+            .eql('com.ia.iconbounce')
+          out.should.haveOwnProperty('name')
+            .eql('IconBounce')
+          out.should.haveOwnProperty('depends')
+            .eql(['mobilesubstrate'])
+          out.should.haveOwnProperty('version')
+            .eql('2.0.0')
+          done()
+        })
+      })
+    })
+
+    describe('non existent file', function() {
+      it('should return an error', function(done) {
+        crw.read('./con', function(err, out) {
+          should.exist(err)
+          err.code.should.eql('ENOENT')
+          done()
+        })
+      })
+    })
+
+    describe('invalid control file', function() {
+      it('should return null, object', function(done) {
+        var controlFile = path.join(__dirname, 'control_invalid')
+        crw.read(controlFile, function(err, out) {
+          should.ifError(err)
+          out.should.be.type('object')
+          out.should.eql({})
+          done()
+        })
+      })
+    })
+  })
+
+  describe('readSync()', function() {
+    describe('valid control file', function() {
+      it('should return an object', function() {
+        var controlFile = path.join(__dirname, 'control')
+        var out = crw.readSync(controlFile)
+        out.should.be.type('object')
+        out.should.haveOwnProperty('package')
+          .eql('com.ia.iconbounce')
+        out.should.haveOwnProperty('name')
+          .eql('IconBounce')
+        out.should.haveOwnProperty('depends')
+          .eql(['mobilesubstrate'])
+        out.should.haveOwnProperty('version')
+          .eql('2.0.0')
+      })
+    })
+
+    describe('non existent file', function() {
+      it('should throw', function() {
+        (function() {
+          var contents = crw.readSync('./con')
+        }).should.throw()
+      })
+    })
+
+    describe('invalid control file', function() {
+      it('should return object', function() {
+        var controlFile = path.join(__dirname, 'control_invalid')
+        var out = crw.readSync(controlFile)
+        out.should.be.type('object')
+        out.should.eql({})
+      })
+    })
+  })
+})
